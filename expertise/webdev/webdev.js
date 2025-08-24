28833441fc5d6a607f3b6841c18f526319943ea7
Adz-Nelo <adz.nelo155@gmail.com>
2025-08-24 23:43:40 +0800

    initial commit

--- expertise/webdev/webdev.js
+++ expertise/webdev/webdev.js
@@ -4,5 +4,7 @@ ScrollReveal({
     duration: 1500,
     delay: 250
   });
-  
+
+ScrollReveal().reveal('img', {origin: 'left'});
+ScrollReveal().reveal('figcaption', {origin: 'right'});
 ScrollReveal().reveal('h1', {origin: 'bottom'});
