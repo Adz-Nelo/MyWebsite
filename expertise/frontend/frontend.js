a4f68d2fc77dd6be59e4096ddce150821ee6f701
Adz-Nelo <adz.nelo155@gmail.com>
2025-08-27 17:01:17 +0800

    initial commit

---
+++ expertise/frontend/frontend.js
@@ -0,0 +1,10 @@
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 1500,
+    delay: 250
+  });
+
+ScrollReveal().reveal('h2', {origin: 'top'})
+ScrollReveal().reveal('lord-icon', {origin: 'right'});
+ScrollReveal().reveal('.portfolio-box', {origin: 'bottom'});
