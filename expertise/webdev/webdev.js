f32ff80626a52966df0928082b820181aa3a112d
Adz-Nelo <adz.nelo155@gmail.com>
2025-09-21 22:09:57 +0800

    initial commit

--- expertise/webdev/webdev.js
+++ expertise/webdev/webdev.js
@@ -5,7 +5,7 @@ ScrollReveal({
     delay: 250
   });
 
-ScrollReveal().reveal('h3, .img-container', {origin: 'top'})
+ScrollReveal().reveal('h3, .img-container, i', {origin: 'top'})
 ScrollReveal().reveal('figcaption, section.discussion figcaption, .book-bind img', {origin: 'right'});
 ScrollReveal().reveal('#base img, #discussion img, #final-defense img', {origin: 'left'});
 ScrollReveal().reveal('h1, .system-review h2, .final-defense , section.infographics p', {origin: 'bottom'});
