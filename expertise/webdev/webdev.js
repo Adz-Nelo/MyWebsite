0764e5add104f4cb7deab70fb9c4b842a0db2bb7
Adz-Nelo <adz.nelo155@gmail.com>
2025-09-28 00:04:03 +0800

    initial commit

--- expertise/webdev/webdev.js
+++ expertise/webdev/webdev.js
@@ -7,5 +7,5 @@ ScrollReveal({
 
 ScrollReveal().reveal('h3, .img-container, i', {origin: 'top'})
 ScrollReveal().reveal('figcaption, section.discussion figcaption, .book-bind img', {origin: 'right'});
-ScrollReveal().reveal('#base img, #discussion img, #final-defense img', {origin: 'left'});
+ScrollReveal().reveal('#base img, #discussion img, #final-defense img, .competition', {origin: 'left'});
 ScrollReveal().reveal('h1, .system-review h2, .final-defense , section.infographics p', {origin: 'bottom'});
