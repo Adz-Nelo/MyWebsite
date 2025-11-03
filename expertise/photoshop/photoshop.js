897ef68400ab4b756b364502dcc1b037ec9c0234
Adz-Nelo <adz.nelo155@gmail.com>
2025-11-03 21:01:51 +0800

    update file

---
+++ expertise/photoshop/photoshop.js
@@ -0,0 +1,10 @@
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 1500,
+    delay: 250
+  });
+
+ScrollReveal().reveal('h2', {origin: 'top'})
+ScrollReveal().reveal('#photoshop-img', {origin: 'right'});
+ScrollReveal().reveal('h1, .portfolio-box', {origin: 'bottom'});
